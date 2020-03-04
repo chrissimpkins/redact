5beb11fc1be972c0c059c461d91b3c1a045a5ce8
Chris Simpkins <git.simpkins@gmail.com>
2020-03-03 22:05:18 -0500

    fmt

--- src/lib/inline.rs
+++ src/lib/inline.rs
@@ -6,7 +6,6 @@ use quote::{quote, ToTokens};
 // use syn::File;
 use syn_inline_mod::{Error as SynInlineError, InlinerBuilder};
 
-
 pub(crate) fn inline_crate(filepath: &Path) -> Result<syn::File, failure::Error> {
     let inline_text: Result<syn::File, SynInlineError> = InlinerBuilder::new()
         .error_not_found(true)
@@ -16,14 +15,16 @@ pub(crate) fn inline_crate(filepath: &Path) -> Result<syn::File, failure::Error>
 
     match inline_text {
         Ok(inline) => Ok(inline),
-        Err(error) => {
-            match error {
-                SynInlineError::NotFound(krate) => {
-                    let (module, sourceloc) = &krate[0];
-                    bail!("Unable to load module '{}' on path '{}'", module, sourceloc.path.display())
-                },
-                _ => bail!("{:?}", error)
+        Err(error) => match error {
+            SynInlineError::NotFound(krate) => {
+                let (module, sourceloc) = &krate[0];
+                bail!(
+                    "Unable to load module '{}' on path '{}'",
+                    module,
+                    sourceloc.path.display()
+                )
             }
-        }
+            _ => bail!("{:?}", error),
+        },
     }
 }
