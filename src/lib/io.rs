1ffc813cf6ac652746500c94d51f3c997a8edc49
Chris Simpkins <git.simpkins@gmail.com>
2020-03-09 01:19:00 -0400

    add ast to rust source writers

--- src/lib/io.rs
+++ src/lib/io.rs
@@ -1,8 +1,10 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 
 use failure::Error;
+use quote::ToTokens;
+// use syn::File;
 // use tempfile::tempdir;
 use tempfile::tempfile;
 
@@ -16,5 +18,15 @@ pub(crate) fn write_tempfile(src: &str) -> Result<File, Error> {
 pub(crate) fn write_file(src: &str, filepath: &PathBuf) -> Result<(), Error> {
     let mut f = File::create(filepath)?;
     f.write_all(src.as_bytes())?;
+    Ok(())
+}
+
+pub(crate) fn write_ast_to_rust(ast: syn::File, filepath: &PathBuf) -> Result<(), Error> {
+    write_file(&ast.into_token_stream().to_string(), filepath)?;
+    Ok(())
+}
+
+pub(crate) fn stdout_ast_to_rust(ast: syn::File) -> Result<(), Error> {
+    print!("{}", &ast.into_token_stream().to_string());
     Ok(())
 }
