4e6521f3e86a90bcebba4df5a865409d4d9635e3
Chris Simpkins <git.simpkins@gmail.com>
2020-03-09 01:20:49 -0400

    add support for comment visitor based comment transforms, new remove method

--- src/lib/transforms/comments.rs
+++ src/lib/transforms/comments.rs
@@ -1,25 +1,38 @@
-use quote::quote;
+use log::{debug, info, trace, warn};
+use proc_macro2::TokenStream;
 use syn::visit_mut::{self, VisitMut};
 use syn::{parse_quote, Attribute, Expr, File, Lit, LitInt};
 
 #[derive(Debug)]
-pub(crate) struct CommentRemove;
+pub(crate) struct Comments;
 
-impl VisitMut for CommentRemove {
+impl Comments {
+
+    // TODO: after parse to ast followed by dump to Rust source, all comments have the following formats:
+    // (1)   #![doc]
+    // (2)   #[doc]
+    pub(crate) fn remove(source: &str) -> String {
+        // replace the instances of `#[doc]`
+        let source_transform_1 = source.replace("#[doc}\n", "").replace("#[doc]", "");
+        // replace the instances of `#![doc]`
+        source_transform_1.replace("#![doc}\n", "").replace("#![doc]", "")
+    }
+
+}
+
+impl VisitMut for Comments {
     fn visit_attribute_mut(&mut self, node: &mut Attribute) {
         // identify documentation lines
         if node.path.is_ident("doc") {
-            // TODO: need to support visitor `visit_attributes_mut(&mut self, node: &mut Vec<Attribute>)`
-            println!("{:?}", node.tokens);
-            println!("{:?}", node.path);
+            debug!("{:?}", node.path);
+            debug!("{:?}", node.tokens);
+            // create empty TokenStream to replace the string literal in
+            // comments.  The comments in formatted Rust source appear as
+            // one of these two formats after this transformation:
+            // (1)  #[doc]
+            // (2)  #![doc]
+            node.tokens = TokenStream::new();  // create empty TokenStream
         }
         visit_mut::visit_attribute_mut(self, node);
     }
-
-    // fn visit_file_mut(&mut self, f: &mut File) {
-    //     for attribute in f.attrs.iter() {
-    //         println!("{:?}", attribute);
-    //     }
-    //     visit_mut::visit_file_mut(self, f);
-    // }
 }
