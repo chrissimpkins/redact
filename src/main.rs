d96d4b9ef2d0ee70de7028701651a87c9046a66f
Chris Simpkins <git.simpkins@gmail.com>
2020-03-13 12:45:14 -0400

    [main] add "Error" to the beginning of stderr error reporting strings

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     env_logger::init();
 
     if let Err(error) = run() {
-        let _ = writeln!(io::stderr(), "{}", error);
+        let _ = writeln!(io::stderr(), "Error: {}", error);
         process::exit(1);
     }
     process::exit(0);
